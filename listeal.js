229824d41c58d09daa2ab8a245d8c217cfb670a7
salihsan <salih.kildis@gmail.com>
2021-06-12 19:48:42 +0300

    Duzeltmeler yapildi enter del calisiyor

--- listeal.js
+++ listeal.js
@@ -1,20 +1,57 @@
 document.querySelector(".ekle").onclick = function () {
-  // ekleme yapılacak listeyi al
   const liste = document.querySelector(".liste");
-  //input elemanina girilen yeni satirin degerini al
   const satir = document.querySelector(".textAl").value;
-  // yeni girilen satiri saklamak icin bir li olusturduk.
   const yeniLi = document.createElement("li");
-  // yeni li icin textnode olusturduk
   const textNode = document.createTextNode(satir);
-  //olusturdugumuz texnode'u yeni li'ye bagladik.
   yeniLi.appendChild(textNode);
-  // yeni eklenen satiri var olan listeye (ul) baglayalim.
   liste.appendChild(yeniLi);
+
+  document.querySelector(".textAl").value = "";
 };
 
+document.querySelector("input").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    document.querySelector(".ekle").click();
+  }
+});
+document.querySelector("input").addEventListener("keyup", function (event) {
+  if (event.keyCode === 46) {
+    document.querySelector(".sil").click();
+  }
+});
 
 document.querySelector(".sil").onclick = function () {
   const liste = document.querySelector(".liste");
   liste.removeChild(liste.lastElementChild);
 };
+
+// let enter = document.querySelector(".ekle").onclick;
+//     enter.addEventListener('keydown', (e) => {
+//       if (e.keyCode === 13) {
+
+//         document.querySelector(".ekle").onclick;
+//       }
+//     });
+
+window.addEventListener("key", checkKeyPress, true);
+
+function checkKeyPress(key) {
+  if (key.keyCode === 13) {
+    document.querySelector(".ekle");
+  }
+
+  if (key.keyCode === 46) {
+    document.querySelector(".sil");
+  }
+}
+
+console.log(checkKeyPress.value);
+// document.querySelector(".ekle").onkeydown = function (event) {
+//   if (event.keyCode == 13) {
+//     document.querySelector(".ekle").click();
+//   }
+
+//   if (event.keyCode == 46) {
+//     document.querySelector(".sil").click();
+//   }
+// };
